9d88a55c6cc978ebb21b45a9120a20bb222aad42
Pietro Nompleggio <pietro.nompleggio@gmail.com>
2016-03-08 12:33:05 +0100

    Initial Commit

---
+++ GhostsInTheCity_Location/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GhostsInTheCity_Location
+//
+//  Created by Pietro Nompleggio on 08/03/16.
+//  Copyright © 2016 Pietro Nompleggio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GhostsInTheCity_Location/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GhostsInTheCity_Location
+//
+//  Created by Pietro Nompleggio on 08/03/16.
+//  Copyright © 2016 Pietro Nompleggio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
